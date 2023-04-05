ff45667d42a22cff9a9900553dc59e41963792b0
torstenbergersen <52758041+torstenbergersen@users.noreply.github.com>
2023-04-05 13:23:52 -0400

    Create basic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add (a, b) {
+  console.log(a + b)
+}
+
+function subtract (a, b) {
+  console.log(a - b)
+}
+
+function multiply (a, b) {
+  console.log(a * b)
+}
+
+function divide (a, b) {
+  console.log (a / b)
+}
